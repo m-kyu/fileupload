139704afb4e3dfe3853f7865782a5349c20b8f3d
chase <53500545+m-kyu@users.noreply.github.com>
2023-02-27 14:57:44 +0900

    asdfsfd

--- pages/write.js
+++ pages/write.js
@@ -4,7 +4,7 @@ const write = () => {
     return (
         <div>
             <h2>글쓰기</h2>
-            <form action="http://localhost:4000/upload" method='post' encType="multipart/form-data">
+            <form action="https://port-0-fileserver-3a9t2ble82e01s.sel3.cloudtype.app/upload" method='post' encType="multipart/form-data">
                 <input type="file" name="fileupload" />
                 <input type="submit" value="저장" />
             </form>
